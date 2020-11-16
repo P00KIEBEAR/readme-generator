13193e8bc6cafa7be47c7bf91a8b218877985aa0
pat paggi <patpaggi@gmail.com>
2020-11-16 01:12:33 -0700

    question might be done?

--- index.js
+++ index.js
@@ -3,104 +3,62 @@
 const { clear } = require('console');
 const inquirer = require('inquirer');
 
-/*
-fs.writeFile('.README.md', err => {
+
+// array of questions for user
+
+// function to write README file
+const writeToFile = (readMe, answers) => {
+  //make varibles!!
+  return inquirer.
+    prompt([
+      /* Pass your questions in here */
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide a description of the project.',
+        default: 'I will come back to fill this out!'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'input',
+        name: 'acknowlledements',
+        message: 'Is there anyone or links that you would like to acknowllede?'
+      }
+
+    ])
+    .then(answers => {
+      //don' forget badge!!
+      /*
+  fs.writeFile('.README.md', err => {
   if (err) {
     console.log(err);
     return;
   }
   console.log('Page created! Check out README.md in this directory to see it!');
-})
-*/
+  })
+  */
 
-// array of questions for user
-
-
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your name!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub Username!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
-  ]);
-};
-
-const promptProject = () => {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)'
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ]);
-};
-// function to write README file
-function writeToFile(fileName, data) {
-  inquirer
-    .prompt([
-      /* Pass your questions in here */
-    ])
-    .then(answers => {
       // Use user feedback for... whatever!!
     })
     .catch(error => {
@@ -113,38 +71,11 @@ function writeToFile(fileName, data) {
 }
 
 
-
-
-
 // function to initialize program
 function init() {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-  return inquirer.questions
 
 }
-promptUser()
-  .then(promptProject)
-  .then(readmeData => {
-    return generatePage(readmeData);
-  })
-  .then(READMEmd => {
-    return writeFile(READMEmd);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
 
 // function call to initialize program
-init();
+//init();
+writeToFile()
