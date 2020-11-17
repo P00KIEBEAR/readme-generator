9d8f80d3b6ada215d50dbd7f8aefec4da31281a7
pat paggi <patpaggi@gmail.com>
2020-11-16 18:33:50 -0700

    answers, linkto page 2

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,61 @@
 
 const fs = require('fs');
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  console.log(answers)
+  fs.writeFile('./README.md', err => {
+    if (err) {
+      console.log(err);
+      return '#' + title
 
-`;
+      /** Table of contents
+      * About the Project
+      * built with
+      * Getting Start
+      * Acknowledements*/
+
+      "## About the Project" + about
+
+      "## Build With" + languages
+
+      /*## Gettting Started
+      
+              ** Screenshot ***/
+
+      "## Acknowlledements" + acknowlledements
+    }
+
+    console.log('Page created! Check out README.md in this directory to see it!');
+  })
+
+
+
 }
 
 module.exports = generateMarkdown;
+
+/*
+BADGE  don't forget badge!!
+'#' + title
+
+* Table of contents
+* About the Project
+* built with
+* Getting Start
+* Acknowledements
+
+'## About the Project' + about
+
+'## Build With' + languages
+
+- HTML
+- CSS
+- javascript
+
+## Gettting Started
+
+**Screenshot**
+
+"## Acknowlledements" + acknowlledements
+1.
+*/
--- index.js
+++ index.js
@@ -2,63 +2,57 @@
 
 const { clear } = require('console');
 const inquirer = require('inquirer');
-
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown')
 
 // array of questions for user
+let questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the name of your project?'
+},
+{
+  type: 'input',
+  name: 'about',
+  message: 'Provide a description of the project.',
+  default: 'I will come back to fill this out!'
+},
+{
+  type: 'checkbox',
+  name: 'languages',
+  message: 'What did you build this project with? (Check all that apply)',
+  choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+},
+{
+  type: 'input',
+  name: 'link',
+  message: 'Enter the GitHub link to your project. (Required)'
+},
+{
+  type: 'confirm',
+  name: 'feature',
+  message: 'Would you like to feature this project?',
+  default: false
+},
+{
+  type: 'input',
+  name: 'acknowlledements',
+  message: 'Is there anyone or links that you would like to acknowllede?'
+  //if I have time add more acknowllede!
+}
 
+]
 // function to write README file
-const writeToFile = (readMe, answers) => {
+const writeToFile = () => {
+
   //make varibles!!
-  return inquirer.
-    prompt([
-      /* Pass your questions in here */
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project?'
-      },
-      {
-        type: 'input',
-        name: 'about',
-        message: 'Provide a description of the project.',
-        default: 'I will come back to fill this out!'
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you build this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
-      },
-      {
-        type: 'input',
-        name: 'acknowlledements',
-        message: 'Is there anyone or links that you would like to acknowllede?'
-      }
+  inquirer.
+    prompt(questions)
 
-    ])
     .then(answers => {
-      //don' forget badge!!
-      /*
-  fs.writeFile('.README.md', err => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Page created! Check out README.md in this directory to see it!');
-  })
-  */
-
+      const { tilte, about, languages, link, feature, acknowlledements } = answers
+      generateMarkdown(answers)
+      // console.log(answers)
       // Use user feedback for... whatever!!
     })
     .catch(error => {
